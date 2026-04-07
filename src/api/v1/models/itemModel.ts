e934c474b402e244118024a775fdfaf8933030bf
Marylen <msaria2@rrc.ca>
2026-04-06 21:34:28 -0500

    added locationModel, modified itemModel - made corrections and added itemStatus choices.

--- src/api/v1/models/itemModel.ts
+++ src/api/v1/models/itemModel.ts
@@ -1,8 +1,10 @@
+export type itemStatus = "lost" | "found" | "claimed";
+
 export interface Item {
   id: string;
   name: string;
   description: string;
   locationId: string;
-  status: "lost" | "found" | "claimed";
-  createdAt: String;
+  status: itemStatus;
+  createdAt: string;
 }
