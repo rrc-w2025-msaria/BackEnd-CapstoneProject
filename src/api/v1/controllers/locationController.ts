27fdb17f97093f63c0e989c2221be55191ce1774
Marylen <msaria2@rrc.ca>
2026-04-06 23:22:07 -0500

    controller file for location created.

---
+++ src/api/v1/controllers/locationController.ts
@@ -0,0 +1,72 @@
+import { Request, Response, NextFunction } from "express";
+import { HTTP_STATUS } from "../../../constants/httpConstants";
+import * as locationService from "../services/locationService";
+import { Location } from "../models/locationModel";
+import { successResponse } from "../models/responseModel";
+
+/**
+ * Manages requests and reponses to retrieve all Items
+ * @param req - The express Request
+ * @param res  - The express Response
+ * @param next - The express middleware chaining function
+ */
+export const getAllLocations = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const locations: Location[] = await locationService.getAllLocations();
+    res
+      .status(HTTP_STATUS.OK)
+      .json(successResponse(locations, "Locations successfully retrieved"));
+  } catch (error: unknown) {
+    next(error);
+  }
+};
+
+export const getLocationById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const id = req.params.id as string;
+    const location: Location = await locationService.getLocationById(id);
+    res
+      .status(HTTP_STATUS.OK)
+      .json(successResponse(location, "Location retrieved"));
+  } catch (error: unknown) {
+    next(error);
+  }
+};
+
+/**
+ * Manages requests, reponses, and validation to create an Item
+ * @param req - The express Request
+ * @param res  - The express Response
+ * @param next - The express middleware chaining function
+ */
+export const createLocation = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    // Extract only the fields we want (destructuring)
+    // const name: string = req.body.name;
+    // const description: string = req.body.description;
+    const { id, name, address } = req.body;
+
+    const newLocation: Location = await locationService.createLocation(
+      id,
+      name,
+      address,
+    );
+    res
+      .status(HTTP_STATUS.CREATED)
+      .json(successResponse(newLocation, "Location created successfully"));
+  } catch (error: unknown) {
+    next(error);
+  }
+};
