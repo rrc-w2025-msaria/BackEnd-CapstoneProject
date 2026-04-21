5973a4f163ac709ca2adb77c0ce04cc2743bd144
Marylen <msaria2@rrc.ca>
2026-04-21 00:03:53 -0500

    implemented authorization and authentication into location routes.

--- src/api/v1/routes/locationRoutes.ts
+++ src/api/v1/routes/locationRoutes.ts
@@ -2,10 +2,12 @@ import express, { Router } from "express";
 import { validateRequest } from "../middleware/validate";
 import { locationSchemas } from "../validations/locationValidation";
 import * as locationController from "../controllers/locationController";
+import authenticate from "../middleware/authenticate";
+import isAuthorized from "../middleware/authorize";
 
 const router: Router = express.Router();
 
-// "/api/v1/items" prefixes all below routes
+// "/api/v1/locations" prefixes all below routes
 router.get("/", locationController.getAllLocations);
 
 router.get(
@@ -16,18 +18,33 @@ router.get(
 
 router.post(
   "/",
+  authenticate,
+  isAuthorized({
+    hasRole: ["manager"],
+    allowSameUser: true,
+  }),
   validateRequest(locationSchemas.createLocation),
   locationController.createLocation,
 );
 
 router.put(
   "/:id",
+  authenticate,
+  isAuthorized({
+    hasRole: ["manager"],
+    allowSameUser: true,
+  }),
   validateRequest(locationSchemas.updateLocation),
   locationController.updateLocation,
 );
 
 router.delete(
   "/:id",
+  authenticate,
+  isAuthorized({
+    hasRole: ["manager"],
+    allowSameUser: true,
+  }),
   validateRequest(locationSchemas.deleteLocation),
   locationController.deleteLocation,
 );
