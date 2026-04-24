a30742a20bcfa68473d9112b3c78233b6c5a7551
Marylen <msaria2@rrc.ca>
2026-04-24 00:35:21 -0500

    locationValidation documentation completed

--- src/api/v1/validations/locationValidation.ts
+++ src/api/v1/validations/locationValidation.ts
@@ -1,5 +1,30 @@
 import Joi from "joi";
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Location:
+ *       type: object
+ *       required:
+ *         - id
+ *         - name
+ *         - address
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The unique identifier for an item
+ *           example: "item_abc123"
+ *         name:
+ *           type: string
+ *           description: The name of the location
+ *           example: "Gym"
+ *         address:
+ *           type: string
+ *           description: Address of the location
+ *           example: "123 Main St"
+ */
+
 export const locationSchemas = {
   createLocation: {
     body: Joi.object({
